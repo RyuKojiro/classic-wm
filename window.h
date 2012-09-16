27c6bfd5504bceed9ad4245ba731ed30753bfe09
Daniel Loffgren <daniel@loffgren.org>
2012-09-16 23:35:58 +0000

    Removing obsoleted window code borrowed from kiosk-wm
    git-svn-id: svn+ssh://svn.phoenixbox.net/svn/classic-wm/trunk@89 eadf1d8b-bcfb-e111-b707-00110a534b34

--- window.h
+++
@@ -1,5 +0,0 @@
-#include <X11/Xlib.h>
-
-int checkWindowSize(Display *display, int screen, Window window, int w, int h);
-void repositionWindow(Display *display, int screen, Window window, int x, int y, int right, int bottom);
-void resizeWindow(Display *display, int screen, Window window, int w, int h);
