1b6968b170d17c9779ccb8d4c08b4bbc084e4ce9
Daniel Loffgren <daniel@loffgren.org>
2016-01-24 19:10:57 -0800

    Resizer has to be destroyed before decorationWindow, since decorationWindow is its parent.

--- decorations.c
+++ decorations.c
@@ -84,10 +84,10 @@ Window decorateWindow(Display *display, Drawable window, Window root, GC gc, int
 }
 
 void undecorateWindow(Display *display, Window decorationWindow, Window resizer) {
-	XUnmapWindow(display, decorationWindow);
-	XDestroyWindow(display, decorationWindow);
 	XUnmapWindow(display, resizer);
 	XDestroyWindow(display, resizer);
+	XUnmapWindow(display, decorationWindow);
+	XDestroyWindow(display, decorationWindow);
 }
 
 void drawDecorations(Display *display, Drawable window, GC gc, const char *title, XWindowAttributes attr) {
